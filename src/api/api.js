72f607b33a3fa5951ec97515b24879bee9b72485
unknown <Alejandro>
2024-01-26 02:01:54 -0400

    change baseUrl api

--- src/api/api.js
+++ src/api/api.js
@@ -2,7 +2,7 @@
 import axios from "axios";
 
 const api = axios.create({
-  baseURL: "http://localhost:5000/api", // Reemplaza con la URL de tu servidor NestJS
+  baseURL: "https://rockola-backend.onrender.com/api", // Reemplaza con la URL de tu servidor NestJS
   timeout: 5000, // Tiempo máximo de espera para las solicitudes
   headers: {
     "Content-Type": "application/json",
