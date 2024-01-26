89483fa1335ef3cfe048237aec18d1a7e100c654
unknown <Alejandro>
2024-01-26 02:42:38 -0400

    rockola companies front

--- src/api/api.js
+++ src/api/api.js
@@ -3,7 +3,6 @@ import axios from "axios";
 
 const api = axios.create({
   baseURL: "https://rockola-backend.onrender.com/api", // Reemplaza con la URL de tu servidor NestJS
-  timeout: 5000, // Tiempo máximo de espera para las solicitudes
   headers: {
     "Content-Type": "application/json",
     // Puedes agregar otros encabezados aquí según tus necesidades
