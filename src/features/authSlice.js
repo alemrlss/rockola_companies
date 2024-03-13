96514c4cf1b6ba7629846609a43bdddf4280311c
unknown <Alejandro>
2024-03-13 02:49:52 -0400

    asd

--- src/features/authSlice.js
+++ src/features/authSlice.js
@@ -1,6 +1,6 @@
 // authSlice.js
 import { createSlice, createAsyncThunk } from "@reduxjs/toolkit";
-import authService from "./authService";
+import authService from "./authService.js";
 
 // Función de verificación al cargar la aplicación
 const checkAuthentication = () => {
